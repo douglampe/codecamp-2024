dba600e79cbd4b8d9f68f30b387cd564b0be3ff0
Doug Lampe <lampedoug@yahoo.com>
2024-02-24 11:52:21 -0500

    feat: add lambda and pipeline

---
+++ demo.js
@@ -0,0 +1,10 @@
+export const handleEvent = async (event) => {
+  return {
+    body: `<html><head><title>${process.env.TITLE}</title></head><body><h1>${process.env.TITLE}</h1></body></html>`,
+    statusCode: 200,
+    headers: {
+      ...headers,
+      'Content-Type': 'text/html',
+    },
+  };
+}
