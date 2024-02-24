d700260261f3fb465c3f25662a0f30c8d04f1e36
Doug Lampe <lampedoug@yahoo.com>
2024-02-24 15:23:41 -0500

    fix: fix handler code

--- demo.js
+++ demo.js
@@ -1,4 +1,4 @@
-export const handleEvent = async (event) => {
+function handleEvent(event) {
   return {
     body: `<html><head><title>${process.env.TITLE}</title></head><body><h1>${process.env.TITLE}</h1></body></html>`,
     statusCode: 200,
